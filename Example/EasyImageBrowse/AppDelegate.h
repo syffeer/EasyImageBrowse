6e5c7a9de26b0b66bd8c2f4d018125649b45977d
MF_sy <sytoby@163.com>
2017-02-15 19:45:01 +0800

    搭建基本框架

---
+++ Example/EasyImageBrowse/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EasyImageBrowse
+//
+//  Created by mofang2 on 17/2/15.
+//  Copyright © 2017年 com.cfd.SaturnEasyTool. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Example/EasyImageBrowse/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EasyImageBrowse
+//
+//  Created by mofang2 on 17/2/15.
+//  Copyright © 2017年 com.cfd.SaturnEasyTool. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
